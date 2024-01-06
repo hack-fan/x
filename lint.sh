c285e0edb48d803bf3c081437a1b15459ba47045
Muninn <hyacinthus@gmail.com>
2024-01-07 07:57:49 +0800

    chore: add lint shell

---
+++ lint.sh
@@ -0,0 +1,37 @@
+
+(
+cd rdb
+golangci-lint run
+)
+(
+cd xdb
+golangci-lint run
+)
+(
+cd xecho
+golangci-lint run
+)
+(
+cd xerr
+golangci-lint run
+)
+(
+cd xlog
+golangci-lint run
+)
+(
+cd xmp
+golangci-lint run
+)
+(
+cd xobj
+golangci-lint run
+)
+(
+cd xtype
+golangci-lint run
+)
+(
+cd xpay
+golangci-lint run
+)
